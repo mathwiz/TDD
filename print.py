add1fa23784199e31a620bb96750a661e55a2c15
Yohan Lee <yohanlee@yahoo.com>
2013-05-27 08:09:21 -0400

    another example file

---
+++ print.py
@@ -0,0 +1,14 @@
+import urllib
+import json
+
+response = urllib.urlopen("http://search.twitter.com/search.json?q=microsoft")
+pyresponse = json.load(response)
+
+#print pyresponse
+#print type(pyresponse)
+#print pyresponse["results"]
+results = pyresponse["results"]
+
+for i in range(10):
+    print "Tweet " + str(i) + ": "+ results[i]["text"]
+
