a853fac45c766c00e5c54a0ca7b24520bde0cf25
mathwiz <mathwiz@gmail.com>
2014-02-17 20:31:25 -0500

    python example

---
+++ python/reference.py
@@ -0,0 +1,14 @@
+def show(alist, name):
+    print(name)
+    print(alist)
+
+
+x = [1, 2, 3, 5, 7, 11]
+y = x
+show(x, "x")
+show(y, "y")
+print ("append to list")
+x.append(13)
+show(x, "x")
+show(y, "y")
+
