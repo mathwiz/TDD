99d5feda3f210adc899a396124cd54f14c189f5b
mathwiz <mathwiz@gmail.com>
2014-02-20 21:20:21 -0500

    adding python example

--- python/class_example.py
+++ python/class_example.py
@@ -1,10 +1,17 @@
 class Example:
         def __init__(self, greeting):
-            self.x = greeting
+                self.g = greeting
 
         def say_it(self, target):
-            return (self.x + ' ' + target)
+                return (self.g + ' ' + target)
+
+        def greeting(self):
+                return self.g
 
 x = Example("Hello")
 print(x.say_it("Yohan"))
+print(x.greeting())
+y = Example("Bye")
+print(y.say_it("dude"))
 
+      
