5e5c10eeabba4988594210449a6695ffb499134d
mathwiz <mathwiz@gmail.com>
2014-10-12 10:40:48 -0400

    bad example

--- racket/sicp/c1/6.rkt
+++ racket/sicp/c1/6.rkt
@@ -3,7 +3,7 @@
   (* x x))
 
 (define (good-enough? guess x)
-  (< (abs (- (square guess) x)) 0.001))
+  (< (abs (- (square guess) x)) 0.0001))
 
 (define (average x y)
   (/ (+ x y) 2))
@@ -19,3 +19,13 @@
 
 (define (sqrt x)
   (sqrt-iter 1.0 x))
+
+(define (new-if predicate then-clause else-clause)
+  (cond (predicate then-clause)
+        (else else-clause)))
+
+(define (sqrt-bad guess x)
+  (new-if (good-enough? guess x)
+          guess
+          (sqrt-bad (improve guess x)
+                  x)))
