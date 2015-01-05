d9217b1835854387489458da76eaa7297a9b0415
mathwiz <mathwiz@gmail.com>
2015-01-04 20:40:53 -0500

    path for emacs

--- lisp/emacs/loadfile.el
+++ lisp/emacs/loadfile.el
@@ -1,5 +1,5 @@
 ;; thie is actually emacs lisp
 (defun loadfile(file) 
 	(load (concat
-		    "/Users/Yohan/Documents/GitHub/TDD/lisp/"
+		    "/Users/Yohan/Documents/GitHub/TDD/lisp/emacs"
 		    file)))
