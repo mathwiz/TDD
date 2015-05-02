bcc1759854bd2c7a9251347e0405f0c87f097533
mathwiz <mathwiz@gmail.com>
2015-05-02 13:27:42 -0400

    folder for sql

---
+++ sql/countries.sql
@@ -0,0 +1,7 @@
+insert into countries values ('US','Unitied States');
+insert into countries values ('UK','Unitied Kingdom');
+insert into countries values ('CA','Canada');
+insert into countries values ('FR','France');
+insert into countries values ('GE','Germany');
+insert into countries values ('MX','Mexico');
+select * from countries;
