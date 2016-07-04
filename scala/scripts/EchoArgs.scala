cb57ed06c833447f22abb60e0ae92ac9364c0dcb
mathwiz <mathwiz@gmail.com>
2016-07-04 17:56:16 -0500

    Scala script

--- scala/scripts/EchoArgs.scala
+++ scala/scripts/EchoArgs.scala
@@ -1,10 +1,4 @@
 /**
   * Created by Yohan on 7/4/2016.
   */
-var i = 0
-while (i < args.length) {
-  if (i != 0) print(" ")
-  print(args(i))
-  i += 1
-}
-println()
+args.foreach(x => println(x))
