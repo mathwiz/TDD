c55a077390da0f65194442b144bd1d76151cdeb3
mathwiz <mathwiz@gmail.com>
2016-10-18 19:25:06 -0400

    append racket

--- racket/appendtolist.rkt
+++ racket/appendtolist.rkt
@@ -1,11 +1,20 @@
 #lang racket
 (define a '(1 2 3))
+(define b '(4 5 6))
 (cdr a)
-(define (append xs e)
+(define (appendelem xs e)
   (cond
     ((empty? xs) (cons e empty))
-    (else (cons (car xs) (append (cdr xs) e)))
+    (else (cons (car xs) (appendelem (cdr xs) e)))
     )
   )
-(append a 99)
-                 
+(appendelem a 99)
+
+(define (appendlists xs ys)
+  (cond
+    ((empty? xs) ys)
+    (else (cons (car xs) (append (cdr xs) ys)))
+    )
+  )
+
+(appendlists a b)
