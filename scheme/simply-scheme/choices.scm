b831ef3a2a5e7fca5a9bc2f07b390e7461ccc1d2
mathwiz <mathwiz@gmail.com>
2016-10-20 20:07:46 -0400

    choices

---
+++ scheme/simply-scheme/choices.scm
@@ -0,0 +1,12 @@
+(define (choices menu)
+  (if (null? menu)
+      '(())
+      (let ((smaller (choices (cdr menu))))
+	(reduce append
+		(map (lambda (item) (prepend-every item smaller))
+		     (car menu))))))
+
+(define prepend-every (lambda (item lst)
+			(map (lambda (choice) (se item choice)) lst)))
+#t
+
