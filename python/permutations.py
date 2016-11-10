7ccf38e8a14a20a692b2644038a4eebee3a3fedf
mathwiz <mathwiz@gmail.com>
2016-11-10 12:40:52 -0500

    permutations

---
+++ python/permutations.py
@@ -0,0 +1,7 @@
+def getPermutation(s, prefix=''):
+        if len(s) == 0:
+                print(prefix)
+        for i in range(len(s)):
+                getPermutation(s[0:i]+s[i+1:len(s)],prefix+s[i] )
+
+getPermutation('abed','')
