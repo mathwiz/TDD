d6272fcb3ff28e84c97ceb4548c871eb58b7c336
mathwiz <mathwiz@gmail.com>
2016-11-10 18:42:58 -0500

    permutations

--- python/permutations.py
+++ python/permutations.py
@@ -1,7 +1,8 @@
 def getPermutation(s, prefix=''):
-        if len(s) == 0:
-                print(prefix)
-        for i in range(len(s)):
-                getPermutation(s[0:i]+s[i+1:len(s)],prefix+s[i] )
+    if len(s) == 0:
+        print(prefix)
+    for i in range(len(s)):
+        getPermutation(s[0:i] + s[i + 1:len(s)], prefix + s[i])
 
-getPermutation('abcd','')
+
+getPermutation('ab', '')
