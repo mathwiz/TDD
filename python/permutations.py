ac47e198352cd20d7ca2ee78c3f20651a17e8a0c
mathwiz <mathwiz@gmail.com>
2016-11-10 20:45:16 -0500

    permutations

--- python/permutations.py
+++ python/permutations.py
@@ -4,5 +4,20 @@ def getPermutation(s, prefix=''):
     for i in range(len(s)):
         getPermutation(s[0:i] + s[i + 1:len(s)], prefix + s[i])
 
+getPermutation('abcd')
 
-getPermutation('ab', '')
+def generatePerm(s):
+    perms = []
+    if len(s) == 0:
+        return perms
+    elif len(s) == 1:
+        perms.append(s[0])
+    else:
+        sublist = generatePerm(s[1:])
+        for x in sublist:
+            for i in range(len(x)):
+                perms.append(x[0:i] + s[0] + x[i:])
+    return perms
+
+
+print(generatePerm('abcd'))
