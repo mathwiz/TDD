27aad0188518f455c072a88f389aab843adbe309
mathwiz <mathwiz@gmail.com>
2016-12-08 22:00:36 -0500

    extended exercises

---
+++ racket/how-to-code/htdp-book/section06/traffic-light.scm
@@ -0,0 +1,18 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname traffic-light) (read-case-sensitive #t) (teachpacks ((lib "draw.rkt" "teachpack" "htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "draw.rkt" "teachpack" "htdp")) #f)))
+;; Traffic Light
+(define WIDTH 50)
+(define HEIGHT 160)
+(define BULB-RADIUS 20)
+(define BULB-DISTANCE 10)
+(define X-BULBS (quotient WIDTH 2))
+(define Y-RED (+ BULB-DISTANCE BULB-RADIUS))
+(define Y-YELLOW (+ Y-RED BULB-DISTANCE (* 2 BULB-RADIUS)))
+(define Y-GREEN (+ Y-YELLOW BULB-DISTANCE (* 2 BULB-RADIUS)))
+
+;; draw with red on
+(start WIDTH HEIGHT)
+(draw-solid-disk (make-posn X-BULBS Y-RED) BULB-RADIUS 'red)
+(draw-circle (make-posn X-BULBS Y-YELLOW) BULB-RADIUS 'yellow)
+(draw-circle (make-posn X-BULBS Y-GREEN) BULB-RADIUS 'green)
