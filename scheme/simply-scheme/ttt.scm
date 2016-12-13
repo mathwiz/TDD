c28d5a1af77782f68f7234a4a802c97ebbe6db2e
mathwiz <mathwiz@gmail.com>
2016-12-12 21:37:52 -0500

    more ttt

--- scheme/simply-scheme/ttt.scm
+++ scheme/simply-scheme/ttt.scm
@@ -10,3 +10,39 @@
   (if (equal? '_ (item square position)) 
       square
       (item square position)))
+
+(define (find-triples position) 
+  (every 
+   (lambda (comb) 
+     (substitute-triple comb position))
+   ALL-WINS))
+
+(define (opponent letter) 
+  (if (equal? letter 'x) 
+      'o
+      'x))
+
+(define (my-pair? triple me) 
+  (and (= (appearances me triple) 2) 
+       (= (appearances (opponent me) triple) 0)))
+
+(define (i-can-win? triples me) 
+  (cond (#f 1) 
+        (#t 2)))
+
+(define (ttt-choose triples me) 
+  (cond (#f (+ 1)) 
+        (#t (+ 2))))
+
+(define (ttt position me) 
+  (ttt-choose (find-triples position) me))
+
+;; TESTS
+(restart 1)
+(define board '_xo_x_o__)
+(define board2 'x_____oxo)
+(substitute-triple 123 board)
+(find-triples board2)
+(opponent 'o)
+(my-pair? 'oox 'o)
+(my-pair? 'oo3 'o)
