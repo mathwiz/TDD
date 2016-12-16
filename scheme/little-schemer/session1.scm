174d3ae91400895d2c1d61dd9071641437a060ef
mathwiz <mathwiz@gmail.com>
2016-12-16 18:16:23 -0500

    more ttt

--- scheme/little-schemer/session1.scm
+++ scheme/little-schemer/session1.scm
@@ -1,3 +1,12 @@
+;; Do this first
+(load "little.scm")
+;; Check the load
 (atom? (quote ()))
 
+;; Ch 1: Toys
+()
+(car ()) ; error
+
+
+;; Utilities
 (restart 1)
--- scheme/simply-scheme/ttt.scm
+++ scheme/simply-scheme/ttt.scm
@@ -32,7 +32,7 @@
                  (my-pair? triple me))
                triples)))
 
-(define (opponent-can-win? triples me)
+(define (opponent-can-win? triples me) 
   (i-can-win? triples (opponent me)))
 
 (define (choose-win winning-triples) 
@@ -40,9 +40,82 @@
       #f
       (keep number? (first winning-triples))))
 
+(define (my-single? triple me) 
+  (and (= (appearances me triple) 1) 
+       (= (appearances (opponent me)) 0)))
+
+(define (extract-digit desired-digit wd) 
+  (keep 
+   (lambda (wd-digit) 
+     (equal? wd-digit desired-digit))
+   wd))
+
+(define (sort-digits digits) 
+  (every 
+   (lambda (digit) 
+     (extract-digit digit digits)) 
+   '(1 2 3 4 5 6 7 8 9)))
+
+(define (repeated-numbers sent) 
+  (every first (keep 
+                (lambda (wd) 
+                  (>= (count wd) 2)) 
+                (sort-digits (accumulate word sent)))))
+
+(define (pivots triples me) 
+  (repeated-numbers (keep 
+                     (lambda (triple) 
+                       (my-single? triple me))
+                     triples)))
+
+(define (first-if-any sent) 
+  (if (empty? sent) 
+      #f
+      (first sent)))
+
+(define (i-can-fork? triples me) 
+  (first-if-any (pivots triples me)))
+
+;; Not helpful so don't use
+;; (define (opponent-can-fork? triples me)
+;;   (i-can-fork? triples (opponent me)))
+
+(define (i-can-advance? triples me) 
+  (best-move (keep 
+              (lambda (triple) 
+                (my-single? triple me))
+              triples) triples me))
+
+(define (best-move my-triples all-triples me) 
+  (if (empty? my-triples) 
+      #f
+      (best-square (first my-triples) all-triples me)))
+
+(define (best-square my-triple triples me) 
+  (best-square-helper (pivots triples (opponent me)) 
+                      (keep number? my-triple)))
+
+(define (best-square-helper opponent-pivots pair) 
+  (if (member? (first pair) opponent-pivots) 
+      (first pair) 
+      (last pair)))
+
+(define (best-free-square triples) 
+  (first-choice (accumulate word triples) 
+                '(5 1 3 7 9 2 4 6 8)))
+
+(define (first-choice possibilities preferences) 
+  (first (keep 
+          (lambda (square) 
+            (member? square possibilities))
+          preferences)))
+
 (define (ttt-choose triples me) 
-  (cond (#f (+ 1)) 
-        (#t (+ 2))))
+  (cond ((i-can-win? triples me)) 
+        ((opponent-can-win? triples me)) 
+        ((i-can-fork? triples me)) 
+        ((i-can-advance? triples me)) 
+        (else (best-free-square triples))))
 
 (define (ttt position me) 
   (ttt-choose (find-triples position) me))
@@ -51,7 +124,9 @@
 (restart 1)
 (define board '_xo_x_o__)
 (define board2 'x_____oxo)
+(define board3 'xo__x___o)
 (substitute-triple 123 board)
+(find-triples board3)
 (find-triples board2)
 (find-triples board)
 (opponent 'o)
@@ -61,3 +136,7 @@
 (i-can-win? (find-triples board2) 'x)
 (i-can-win? (find-triples board) 'x)
 (opponent-can-win? (find-triples board) 'o)
+(repeated-numbers (find-triples board3))
+(find-triples board2)
+(extract-digit 7 "4x6x473x7")
+(ttt '_________ 'x)
