f9f12bb6ba0b98a7214bb0521cf1bde9f25ddc71
mathwiz <mathwiz@gmail.com>
2016-12-17 16:26:58 -0500

    rearranging words

---
+++ racket/how-to-code/htdp-book/section12/rearranging-words.rkt
@@ -0,0 +1,6 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname rearranging-words) (read-case-sensitive #t) (teachpacks ((lib "draw.rkt" "teachpack" "htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "draw.rkt" "teachpack" "htdp")))))
+;; word -> list-of-words
+;; Produce a list of all rearrangements of the letters in a-word
+(define (arrangements a-word) empty) ; stub
