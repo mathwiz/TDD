0f02409e9025da65e8102dcf9ebd1f59dbec78d1
mathwiz <mathwiz@gmail.com>
2016-12-18 22:27:51 -0500

    more on this

--- racket/how-to-code/htdp-book/section12/rearranging-words.rkt
+++ racket/how-to-code/htdp-book/section12/rearranging-words.rkt
@@ -10,6 +10,24 @@
         (else ... (first a-word) ... (arrangements (rest a-word)) ...)))
 
 (define (arrangements a-word)
-  (cond ((empty? a-word) ...)
-        (else ... (first a-word) ... (arrangements (rest a-word)) ...)))
+  (cond ((empty? a-word) empty)
+        (else (insert-everywhere/in-all-words (first a-word) (arrangements (rest a-word))))))
 
+(define L1 (cons (cons 'e (cons 'r empty)) 
+                 (cons (cons 'r (cons 'e empty)) 
+                       empty)))
+(define L1+d (list (list 'd 'e 'r) (list 'e 'd 'r) (list 'e 'r 'd) (list 'd 'r 'e) (list 'r 'd 'e) (list 'r 'e 'd)))
+
+;; symbol list-of-words -> list-of-words
+;; Produce a list where the symbol is inserted between all letters and at the beginning and end.
+;(check-expect (insert-everywhere/in-all-words 'd L1) empty)
+
+;(define (insert-everywhere/in-all-words s ws) empty) ; stub
+  
+(define (insert-everywhere/in-all-words s ws)
+  (cond ((empty? ws) empty)
+        (else (cons (append (list s) (first ws)) (insert-everywhere/in-all-words s (rest ws))))))
+
+(define (permute s letters)
+  (cond ((empty? letters) (append (list s) empty))
+        (else (cons (append (list s) letters) (permute s (rest letters))))))
