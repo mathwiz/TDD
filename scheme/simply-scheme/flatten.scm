5930d9fb2aad415f82e55691f7fcc0881b49b3f6
mathwiz <yohanlee@yahoo.com>
2017-01-03 14:44:07 -0500

    flatten in scheme

---
+++ scheme/simply-scheme/flatten.scm
@@ -0,0 +1,18 @@
+;; ListOfListOrAtom is one of:
+;; - empty
+;; - atomic distinct
+;; - compound: (cons ListOfListOrAtom ListOfListOrAtom)
+(define LLA1 '())
+(define LLA2 (cons 2 ()))
+(define LLA3 (cons 3 (cons LLA2 (cons 1 ()))))
+(define LLA4 (cons 0 (cons LLA2 (cons -1 (cons LLA3 (cons 1 ()))))))
+
+;; ListOfListOrAtom -> ListOfAtom
+;;(define (flatten xs) '()) ; stub
+(define (flatten xs)
+  (cond ((empty? xs) '())
+        ((word? xs) (cons xs ()))
+        (else (cons (flatten (car xs)) (flatten (cdr xs))))))
+
+
+(restart 1)
