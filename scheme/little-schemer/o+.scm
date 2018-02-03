c75b8812260775b1062234c9009edcc44a44f94f
mathwiz <mathwiz@gmail.com>
2018-02-03 14:27:58 -0500

    custom versions of + and -

---
+++ scheme/little-schemer/o+.scm
@@ -0,0 +1,9 @@
+;; Natural Natural -> Natural
+;; version of binary + using sub1 and add1
+(define o+ 
+  (lambda (n m) 
+    (cond ((zero? m) n) 
+          (else
+           (add1 (o+ n (sub1 m)))))))
+
+
