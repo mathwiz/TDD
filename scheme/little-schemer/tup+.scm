6349becf0e6d64b8632fe70eb2257ed852529c40
mathwiz <mathwiz@gmail.com>
2018-02-03 16:52:25 -0500

    little schemer

--- scheme/little-schemer/tup+.scm
+++ scheme/little-schemer/tup+.scm
@@ -1,15 +1,12 @@
 ;; Tuple Tuple -> Tuple
 ;; produce the pairwise addition of 2 tuples of the same length
 (define (tup+ a b) 
-  (cond ((and 
-          (null? a) 
-          (null? b)) 
-         (quote ())) 
-        (else (cons
-               (o+ (car a) 
-                   (car b))
-               (tup+ (cdr a) 
-                     (cdr b))))))
+  (cond ((null? a) b)
+        ((null? b) a) 
+        (else (cons (o+ (car a) 
+                        (car b)) 
+                    (tup+ (cdr a) 
+                          (cdr b))))))
 
 
 
