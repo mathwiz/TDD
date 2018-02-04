c6091fdfcad47b9d990d72c4aed45f5b0efc1ee9
mathwiz <mathwiz@gmail.com>
2018-02-04 09:56:19 -0500

    < and >

--- scheme/little-schemer/little.scm
+++ scheme/little-schemer/little.scm
@@ -104,3 +104,25 @@
 
 
 
+;; Natural Natural -> Boolean
+;; produce true if n > m
+(define (o> n m)
+  (cond ((zero? n) #f)
+        ((zero? m) #t)
+        (else
+         (o> (sub1 n)
+             (sub1 m)))))
+
+
+
+;; Natural Natural -> Boolean
+;; produce true if n < m
+(define (o< n m)
+  (cond ((zero? m) #f)
+        ((zero? n) #t)
+        (else
+         (o< (sub1 n)
+             (sub1 m)))))
+
+
+
