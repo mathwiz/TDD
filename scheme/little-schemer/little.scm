5efa3163f5d33fe5afcec3ee25085542d7abf1b1
mathwiz <mathwiz@gmail.com>
2018-02-04 15:22:14 -0500

    no-nums

--- scheme/little-schemer/little.scm
+++ scheme/little-schemer/little.scm
@@ -177,3 +177,16 @@
 
 
 
+;; (listof X) -> (listof X)
+;; produce a list that does not contain any numbers
+(define (no-nums lox) 
+  (cond ((null? lox) 
+         (quote ())) 
+        ((number? (car lox)) 
+         (no-nums (cdr lox)))  
+        (else (cons (car lox) 
+                    (no-nums (cdr lox))))))
+
+
+
+
