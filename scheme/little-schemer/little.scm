47ff22c61571260126a6391537afe076d687ebff
mathwiz <mathwiz@gmail.com>
2018-02-04 20:26:44 -0500

    rember-star

--- scheme/little-schemer/little.scm
+++ scheme/little-schemer/little.scm
@@ -229,3 +229,19 @@
 
 
 
+;; Atom (listof X) -> (listof X)
+;; produce list where a is removed from all sublists
+(define (rember* a l) 
+  (cond ((null? l) 
+         (quote ())) 
+        ((atom? (car l)) 
+         (cond ((eq? (car l) a) 
+                (rember* a (cdr l))) 
+               (else (cons (car l) 
+                           (rember* a (cdr l))))))
+        (else (cons (rember* a (car l)) 
+                    (rember* a (cdr l))))))
+
+
+
+
