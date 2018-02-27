c289eeb71410eec32c54280c2da4be6a8be1d2a7
mathwiz <mathwiz@gmail.com>
2018-02-27 17:20:35 -0500

    insertL*

--- scheme/little-schemer/little.scm
+++ scheme/little-schemer/little.scm
@@ -261,3 +261,18 @@
 
 
 
+;; Atom Atom (listof X) -> (listof X)
+;; insert new to the left of old in all sublists of l
+(define (insertL* old new l) 
+  (cond ((null? l) 
+         (quote ())) 
+        ((atom? (car l)) 
+         (cond ((eq? (car l) old) 
+                (cons new (cons old (insertL* old new (cdr l))))) 
+               (else (cons (car l) 
+                           (insertL* old new (cdr l)))))) 
+        (else (cons (insertL* old new (car l)) 
+                    (insertL* old new (cdr l))))))
+
+
+
