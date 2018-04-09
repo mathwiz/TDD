1108fe0993f1e8ae1d33442f05192a32e459d224
Yohan Lee <mathwiz@gmail.com>
2018-04-09 16:22:40 -0400

    recursion

--- clojure/apps/noob/src/noob/core.clj
+++ clojure/apps/noob/src/noob/core.clj
@@ -2,10 +2,15 @@
   (:gen-class))
 
 (defn -main "I don't do a whole lot ... yet." [& args] 
-  (println (greeting (str (first args) "!"))))
+  (println (greeting (str (stringify args) "!"))))
 
 (defn train [] 
   (println "Choo choo!"))
 
-(defn greeting [whom]
+(defn greeting [whom] 
   (str "Hello" ", " whom))
+
+(defn stringify [args] 
+  (cond (empty? args) "" 
+        :else (str (first args) " " 
+                   (stringify (rest args)))))
