ae1bbb8ec4374e00579c8020d7aa85e2d8bd14fe
Yohan Lee <mathwiz@gmail.com>
2018-04-09 14:30:00 -0400

    more clojure

--- clojure/apps/noob/src/noob/core.clj
+++ clojure/apps/noob/src/noob/core.clj
@@ -2,7 +2,10 @@
   (:gen-class))
 
 (defn -main "I don't do a whole lot ... yet." [& args] 
-  (println "Hello, Yohan!"))
+  (println (greeting (str (first args) "!"))))
 
 (defn train [] 
   (println "Choo choo!"))
+
+(defn greeting [whom]
+  (str "Hello" ", " whom))
