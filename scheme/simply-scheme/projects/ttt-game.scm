b9e966279a7d51e73f3b91d27e9969d3bd41a702
mathwiz <mathwiz@gmail.com>
2018-05-25 18:19:02 -0400

    pretty print tic tac toe

--- scheme/simply-scheme/projects/ttt-game.scm
+++ scheme/simply-scheme/projects/ttt-game.scm
@@ -33,9 +33,11 @@
 (define play-ttt-helper 
   (lambda (x-strat o-strat position whose-turn) 
     (cond ((position-won? position (opponent whose-turn)) 
-           (list (opponent whose-turn) 'wins!)) 
+           (begin (print-position position) 
+                  (list (opponent whose-turn) 'wins!))) 
           ((completed-position? position) 
-           '(tie game)) 
+           (begin (print-position position) 
+                  '(tie game))) 
           (else 
            (let ((square 
                   (if (equal? whose-turn 'x) 
@@ -51,21 +53,46 @@
            (word letter (bf position))) 
           ((number? square) 
            (word (first position) 
-                 (add-move (- square 1) letter (bf position))))
+                 (add-move (- square 1) letter (bf position)))) 
           (else (location '_ position)))))
 
 
-(define ask-user
-  (lambda (position letter)
-    (print-position position)
-    (display letter)
-    (display "'s move: ")
+(define ask-user 
+  (lambda (position letter) 
+    (print-position position) 
+    (display letter) 
+    (display "'s move: ") 
     (read)))
 
 
-(define print-position
-  (lambda (position)
-    (show position)))
+(define print-position 
+  (lambda (position) 
+    (print-row (subword position 1 3)) 
+    (show "-+-+-") 
+    (print-row (subword position 4 6)) 
+    (show "-+-+-") 
+    (print-row (subword position 7 9)) 
+    (newline)))
 
 
+(define print-row 
+  (lambda (row) 
+    (maybe-display (first row)) 
+    (display "|") 
+    (maybe-display (first (bf row))) 
+    (display "|") 
+    (maybe-display (last row)) 
+    (newline)))
 
+
+(define maybe-display 
+  (lambda (letter) 
+    (if (not (equal? letter '_)) 
+        (display letter) 
+        (display " "))))
+
+
+(define subword 
+  (lambda (wd start end) 
+    ((repeated bf (- start 1)) 
+     ((repeated bl (- (count wd) end)) wd))))
