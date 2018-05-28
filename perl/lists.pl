f347e50b316799de704d898da540f2cbce0435b0
mathwiz <mathwiz@gmail.com>
2018-05-27 22:00:00 -0400

    list args

--- perl/lists.pl
+++ perl/lists.pl
@@ -1,11 +1,19 @@
-@nums = (1,2,3,4,5,6);
+@nums = (1,2,3,4,5,6,7);
 
 @nums = (0, @nums);
 
-print @nums[0] . "\n";
+print @nums[1] . "\n";
 
-showarray(@nums);
-showarray(double(@nums));
+showarray (@nums);
+showarray (double(@nums));
+print "Sum: " . sum (0,(1,2,3,4,5,6))  . "\n";
+
+sub sum {
+    my ($acc, @list) = @_;
+    if (! @list) { return $acc; }
+    my ($head, @tail) = @list;
+    return sum ($acc + $head, @tail);  
+}
 
 sub double {
     if (! @_) { return (); }
