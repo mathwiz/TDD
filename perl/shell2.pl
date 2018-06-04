6077b66ae6635349b52184d40beaad9f0e575c81
mathwiz <mathwiz@gmail.com>
2018-06-04 16:37:39 -0400

    history

--- perl/shell2.pl
+++ perl/shell2.pl
@@ -3,19 +3,53 @@ use feature qw(say);
 
 my $prompt = "\nperl shell> ";
 my $exp = '';
+my @history = ();
+
 while (
        do {
          print $prompt;
-         chomp($exp = <STDIN>)
+         chomp($exp = <STDIN>);
+         $exp;
         }) {
-  if ( $exp =~ /\s*load\s*\(\s*['"](\w+)['"]\s*\)/ ) {
+  if ($exp eq 'history' or $exp eq 'h') {
+    showHistory();
+  } elsif ($exp eq '!') {
+    history(0);
+  } elsif ($exp =~ /!(\d+)/) {
+    history($1 - 1);
+  } elsif ( $exp =~ /\s*load\s*\(\s*['"](\w+)['"]\s*\)/ ) {
     load ($1);
-  } elsif ($exp eq 'quit') {
+    push (@history, $exp);
+  } elsif ($exp eq 'quit' or $exp eq 'exit') {
     last;
   } else {
     eval $exp;
+    push (@history, $exp);
   }
   print $@; # any errors
+}
+
+
+sub showHistory {
+  my @h_list = @history;
+  my $index = @history;
+  for $it (@h_list) {
+    say $index--, " ",  $it;
+  }
+}
+
+sub history {
+  my $offset = shift;
+  my $index = @history - 1 - $offset;
+  my $cmd = $history[$index];
+  if (@history == 0) {
+    say "History empty";
+  } elsif ($cmd) {
+    say $cmd;
+    eval $cmd;
+  } else {
+    say "Bad command: ", $index;
+  }
 }
 
 sub load {
