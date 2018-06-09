df78c807de65ebe44d42cd6265b3e19a79ec8c34
mathwiz <mathwiz@gmail.com>
2018-06-09 16:59:19 -0400

    =>

--- perl/show_structures.pl
+++ perl/show_structures.pl
@@ -1,3 +1,3 @@
-sub showHash { my %h = @_; foreach $it (keys %h) {print "$it : $h{$it}\n"} }
+sub showHash { my %h = @_; foreach $it (keys %h) {print "$it => $h{$it}\n"} }
 
 sub showArray { foreach $it (@_) {print "$it "} print "\n";}
