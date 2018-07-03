85bf039cbf88c2a83b1e98b72471332fe58247fd
mathwiz <mathwiz@gmail.com>
2018-07-03 13:14:41 -0400

    ruby example

--- ruby/headfirst/get_number.rb
+++ ruby/headfirst/get_number.rb
@@ -1,8 +1,44 @@
 # Get My Number Game
 
-puts "Welcome to 'Get My Number!'"
+puts "Welcome to 'Get My Number'!"
 print "What's your name? "
 
 input = gets
 
-puts "Welcome, #{input}"
+name = input.chomp
+
+puts "Welcome, #{name}!"
+
+# Store the target
+limit = 100
+puts "\nI've got a random number between 1 and #{limit}."
+puts "Can you guess it?"
+target = rand(limit) + 1
+
+guess_limit = 7
+num_guesses = 0
+guessed_it = false
+
+while num_guesses < guess_limit && (not guessed_it)
+
+	puts "You've got #{guess_limit - num_guesses} gueses left."
+	print "Make a guess: "
+	guess = gets.to_i
+
+	num_guesses += 1
+
+	if guess < target
+		puts "Oops. Your guess was LOW."
+	elsif guess > target
+		puts "Oops. Your guess was HIGH."
+	else
+		puts "\nGood job, #{name}!"
+		puts "You guessed my number in #{num_guesses} guesses!"
+		guessed_it = true
+	end
+
+end
+
+unless guessed_it
+	puts "\nSorry. You didn't get my number. (It was #{target}.)"
+end
