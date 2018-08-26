627aaff7a93e795e2a0b0909d72a6e615957d59a
mathwiz <mathwiz@gmail.com>
2018-08-26 11:10:38 -0400

    does not work

--- groovy/InterceptInteger.groovy
+++ groovy/InterceptInteger.groovy
@@ -1,7 +1,25 @@
 Integer.metaClass.invokeMethod = { String name, args ->
-  System.out.println("Call to $name intercepted  on $delegate...")
-  
-  def validatedMethod = Integer.metaClass.getMetaMethod(name, args)
+    System.out.println("Call to $name intercepted  on $delegate...")
+
+    def validatedMethod = Integer.metaClass.getMetaMethod(name, args)
+
+    if (validatedMethod == null) return Integer.metaClass.invokeMissingMethod(delegate, name, args)
+
+    System.out.println("running pre-filter...")
+
+    result = validatedMethod.invoke(delegate, args)
+
+    System.out.println("running post-filter...")
+
+    result
 }
 
+//This blows up
+println 4.floatValue()
+println 4.intValue()
 
+try {
+    println 4.empty()
+} catch(Exception e) {
+    println e
+}
