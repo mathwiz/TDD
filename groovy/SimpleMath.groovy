b5449bee5b2b78e40255f1d90829dffe868d1a9c
mathwiz <mathwiz@gmail.com>
2018-09-06 21:09:18 -0400

    formatting

--- groovy/SimpleMath.groovy
+++ groovy/SimpleMath.groovy
@@ -1,17 +1,18 @@
 class SimpleMath {
-	def static fibonacci(n) { n }
+  def static fibonacci(n) { n }
 
-	def static powerOf2(n)	{ 2.power(n) }
+  def static powerOf2(n)	{ 2.power(n) }
 }
 
 
 class SimpleMathTest {
-	def static main(String[] args) {
-		println SimpleMath.powerOf2(5)
-	}
+  def static main(String[] args) {
+    println SimpleMath.powerOf2(5)
+  }
 }
 
 def simple_test() {
-	println "Test of fibonacci"
-	println SimpleMath.fibonacci(5)
+  println "Test of fibonacci"
+  println SimpleMath.fibonacci(5)
 }
+
