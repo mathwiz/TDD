9fa0c6d456419c6fe61042512681d82656d6e210
mathwiz <yohanlee@yahoo.com>
2018-11-14 19:45:18 -0500

    racket gui

---
+++ racket/gui/frame.rkt
@@ -0,0 +1,9 @@
+#lang racket/gui
+
+(require racket/gui/base)
+ 
+; Make a frame by instantiating the frame% class
+(define frame (new frame% [label "Example"]))
+ 
+; Show the frame by calling its show method
+(send frame show #t)
