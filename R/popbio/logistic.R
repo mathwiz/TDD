0f93e079fc4e04a5217869286a6c803189f01c6a
Yohan Lee <mathwiz@gmail.com>
2019-03-14 12:46:00 -0400

    logistic in R

---
+++ R/popbio/logistic.R
@@ -0,0 +1,16 @@
+logisticVec <- function(n, p0, r) {
+    times = 1:(n-1)
+    p = rep(0, n)
+    p[1] = p0
+    for (i in times) {
+        p[i+1] = r * p[i] * (1 - p[i])
+    }
+    p
+}
+
+logisticPlot <- function(n, p0, r) {
+    times = 0:(n-1)
+    pops = logisticVec(n, p0, r)
+    plot(times, pops, type="n")
+    lines(times, pops, type="l")
+}
