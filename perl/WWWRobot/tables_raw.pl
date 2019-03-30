d9e48846ffefca8ebda003cc78d31eaa6190103d
mathwiz <yohanlee@yahoo.com>
2019-03-29 21:04:29 -0400

    extract raw tables

--- perl/WWWRobot/tables_raw.pl
+++ perl/WWWRobot/tables_raw.pl
@@ -2,6 +2,18 @@
 use strict;
 use HTML::TableExtract;
 
-my $html_string = do { local $/; <STDIN> };
+#my $html_string = do { local $/; <STDIN> };
+#print $html_string; 
 
-print $html_string; 
+my $in_table = 0;
+my $line = '';
+foreach $line (<>) {
+    if ($line =~ /<\/table/) { 
+        print $line; 
+        $in_table = 0; 
+    }
+    if ($line =~ /<table/) { 
+        $in_table = 1; 
+    }
+    $in_table and print $line;
+}
