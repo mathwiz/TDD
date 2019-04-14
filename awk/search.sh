75196cd92403b3c55105e0a4d3cd7c0442ba4901
mathwiz <mathwiz@gmail.com>
2019-04-14 10:19:31 -0400

    what's wrong?

--- awk/search.sh
+++ awk/search.sh
@@ -4,6 +4,6 @@ bib="bibliography.txt"
 cmd='BEGIN { FS="\n"; RS="" } /'
 cmd+=$1
 cmd+='/ { print; print ""; }'
-echo "'$cmd' $bib"
-awk $cmd $bib
-
+echo $"'$cmd'"
+awk $"'$cmd'" $bib
+#awk $"\'BEGIN { FS=\'\n\'; RS=\'\' } /$1/ { print; print \'\'  }\'" $bib
