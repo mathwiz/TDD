775aa5672f87ca51aadf4626667540e6bde92de9
mathwiz <mathwiz@gmail.com>
2019-08-03 19:45:20 -0400

    perceptron

--- python/perceptron/matrixnet.py
+++ python/perceptron/matrixnet.py
@@ -1,23 +1,33 @@
-import numpy as np
 import random
 
 
 def sign(x):
     return -1 if x < 0 else 1
 
-def nextweight(x, w, c, d):
-    return c * (d - sign(x * w)) * x
+def learningrule(xs, weights, target, rate, bias):
+    return [ w + rate * (target - sign(x*w)) * x
+                 for x, w in zip(xs + [bias], weights) ]
+
+def deltaweights(xs, w, target, rate=1, bias=1):
+    products = [ w[i] * val for i, val in enumerate(xs + [bias]) ] 
+    result = sign(sum(products))
+    return w if result == target else learningrule(xs, w, target, rate, bias)
     
-
 def train(data, network, size):
-    for r in data:
-        print(r[0:size], r[size])
+    weights = network
+    for row in data:
+        inputs = row[0:size]
+        goal = row[size]
+        weights = deltaweights(inputs, weights, goal)
+        print(inputs, goal, weights)
+    return weights
 
+        
 bias = 1
+weights = [ random.random(), random.random(), random.random() ]
+#weights = [random.random(), random.random()]
 
-network = np.array([1, 1])
-
-data = np.array([
+data = [
     [1.0, 1.0, 1],
     [9.4, 6.4, -1],
     [2.5, 2.1,  1],
@@ -28,10 +38,35 @@ data = np.array([
     [2.8, 0.8, 1],
     [1.2, 3.0, 1],
     [7.8, 6.1, -1],
-    ])
+    ]
+
+tests = [
+    [7, 5],
+    [1, 9],
+    [5, 4],
+    [5, 6],
+    [.4, 3],
+    [2, 1.1],
+    [2, 8.7],
+    [4, 2],
+    [4, 8],
+    [0, 0],
+    [10, 10]
+]
 
 #print(data)
-print(network * 2)
+#print(weights)
+#print(deltaweights(network, weights, 2, -1))
+#print([y for x, y in zip([1,2,3], [.1,.2,.3])])
+#print(sum([1,2,3]))
+#print(learningrule(data[0][0:2], weights, target=-1, rate=1, bias=1))
 
-train(data, None, 2)
+newweights = train(data, weights, 2)
+print(newweights)
+print("tests")
+for test in tests:
+    f = sum([ x * w for x, w in zip(test + [1], newweights) ])
+    print(test, f, sign(f))
+
+#train(data, None, 2)
 
