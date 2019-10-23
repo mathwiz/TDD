f17e523d5dd32be5bac1d8c0af00bf8bb66c295d
Yohan Lee <yohanlee@yahoo.com>
2019-10-22 22:31:35 -0400

    sicp example

---
+++ racket/multiply.rkt
@@ -0,0 +1,20 @@
+#lang racket
+
+(define (halve n)
+  (/ n 2))
+
+(define (double n)
+  (* n 2))
+
+(define (even? x)
+  (= (remainder x 2) 0))
+
+(define (mult-invariant a b acc)
+  (cond ((= b 0) acc)
+        ((even? b) (mult-invariant (double a) (halve b) acc))
+        (else (mult-invariant a (sub1 b) (+ acc a)))))
+
+(define (times a b)
+  (mult-invariant a b 0))
+
+        
