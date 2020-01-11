3dc9fa4490cb5598a22abc7d7df4db3c947ce204
Yohan Lee <yohanlee@yahoo.com>
2020-01-11 06:44:06 -0500

    space

--- racket/flatten.rkt
+++ racket/flatten.rkt
@@ -8,7 +8,7 @@
 (define xs (cons a (cons 0 empty)))
 ;xs
 
-(define ys (cons 0(cons a (cons -1 (cons (cons b (cons 99 (cons c empty))) (cons -2 (cons -3 empty)))))))
+(define ys (cons 0 (cons a (cons -1 (cons (cons b (cons 99 (cons c empty))) (cons -2 (cons -3 empty)))))))
 ;ys
 
 (define (flatten xs) 
