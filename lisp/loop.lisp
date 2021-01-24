76f93c923b9256551da4850221e2b58f86a70cb5
Yohan Lee <yohanlee@yahoo.com>
2021-01-24 10:52:00 -0500

    clean up

--- lisp/loop.lisp
+++ lisp/loop.lisp
@@ -10,6 +10,16 @@
 (setf (gethash 'b h) 2)
 (loop for k being the hash-key of h do (print k))
 (loop for v being the hash-value of h do (print v))
+(loop for k being the hash-key using (hash-value v) of h do (format t "~a ~a~%" k v))
+
+(loop for i from 1 to 3 do (print i))
+
+(loop for i from 1.0 to 3.0 by 0.5 do (print i))
+
+(loop for i from 3 downto 1 do (print i))
+
+
+
 
 
 
