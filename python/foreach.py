5baac865309ffc8e6df3fa9dd3ecf7c268be80f9
Yohan Lee <yohanlee@yahoo.com>
2021-02-06 20:32:36 -0500

    python3

--- python/foreach.py
+++ python/foreach.py
@@ -2,5 +2,5 @@ from random import randint
 
 n = 20
 comp = [randint(0, 1) for _ in range(n)] 
-print "List of", len(comp), "random numbers"
-print comp
+print("List of", len(comp), "random numbers")
+print(comp)
