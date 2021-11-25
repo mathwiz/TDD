0f7ef665b5c92149091664abfa322f0d7f136d70
Yohan Lee <yohanlee@yahoo.com>
2021-11-25 11:49:37 -0500

    random

---
+++ haskell/coin_flip.hs
@@ -0,0 +1,14 @@
+import System.Random
+
+data Coin = Heads | Tails deriving (Show, Enum, Bounded)
+
+instance Random Coin where
+  randomR (a, b) g =
+    case randomR (fromEnum a, fromEnum b) g of
+      (x, g') -> (toEnum x, g')
+  random g = randomR (minBound, maxBound) g
+
+main = do
+  g <- newStdGen
+  print . take 10 $ (randoms g :: [Coin])
+
