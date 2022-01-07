e4588093d4e9e7cb35e736331ab95ccb45468f3f
Yohan Lee <yohanlee@yahoo.com>
2022-01-06 19:27:53 -0500

    filter

--- lisp/filter.lisp
+++ lisp/filter.lisp
@@ -1,7 +1,9 @@
-(defun for-lines ()
+(defun for-lines (n)
   (let ((line (read-line)))
       (if (> (length line) 0)
           (progn 
-            (format t "~S~%" line)
-            (for-lines))
+            (format t "~d: ~d~%" n line)
+            (for-lines (1+ n)))
           (format t "~%"))))
+
+(for-lines 1)
