148e8fba183d7646222f47c2cb8c83366c61a40b
Yohan Lee <yohanlee@yahoo.com>
2022-01-29 16:35:36 -0500

    haskell

---
+++ haskell/printf_example.hs
@@ -0,0 +1,11 @@
+import Text.Printf
+
+main = do
+  let xs = [1,2,3]
+  let ys = ['f','a','l','l']
+  printf "length %s => %d\n" (show xs) (length xs)
+  printf "length %s => %d\n" (show ys) (length ys)
+
+  
+
+
