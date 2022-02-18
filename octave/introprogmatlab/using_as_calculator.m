80df8314222e1e6750add7be79cf2376d31f086e
Yohan Lee <yohanlee@yahoo.com>
2022-02-18 10:25:02 -0500

    matlab class

---
+++ octave/introprogmatlab/using_as_calculator.m
@@ -0,0 +1,13 @@
+speed_light_kps = 300000
+year_sec = 365 * 24 * 60 * 60
+lightyear_km = year_sec * speed_light_kps
+earth_to_sun_km = 150e6
+earth_to_sun_sec = earth_to_sun_km / speed_light_kps
+earth_to_sun_minutes = earth_to_sun_sec / 60
+earth_to_moon_km = 384400
+earth_to_moon_sec = earth_to_moon_km / speed_light_kps
+abs_zero_C = -273.15
+abs_zero_F = abs_zero_C * 9 / 5 + 32
+paper_ignition_F = 451
+funny_new_temp_defined_on_multiple_lines_in_C = abs_zero_C + 23 * 1.00000008...
+- abs_zero_C
