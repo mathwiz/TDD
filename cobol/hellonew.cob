a58078b0da33c05a5845b582bc7226a96138f0a3
Yohan Lee <yohanlee@yahoo.com>
2022-04-14 17:01:24 -0400

    hello world

---
+++ cobol/hellonew.cob
@@ -0,0 +1,9 @@
+       *> Sample GNUCOBOL program
+       identification division.
+       program-id. hellonew.
+       procedure division.
+       display
+           "Hello, new world!"
+       end-display
+       goback.
+           
