1cfed1445fa6ecb48ec7630fd5d5dc866aa26fc8
Yohan Lee <yohanlee@yahoo.com>
2022-06-08 11:39:12 -0400

    node

--- javascript/hello.js
+++ javascript/hello.js
@@ -1,6 +1,6 @@
 Hello = function() { console.log("Hello from Node!") };
 Hello();
-Goodbye = function() { console.log('end of program') );
+Goodbye = function() { console.log('end of program') };
 
 module.exports.Hello = Hello;
 module.exports.GB = Goodbye;
