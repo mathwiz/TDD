63471aac2441243d94b2b574e2577cdbbeec0826
Yohan Lee <yohanlee@yahoo.com>
2023-01-01 14:59:06 -0500

    command line Java

---
+++ java/hello/World.java
@@ -0,0 +1,11 @@
+package hello;
+
+import java.io.*;
+
+public class World {
+    static PrintStream out = System.out;
+
+    public static void main(String[] args) {
+        out.println("Hello World!");
+    }
+}
