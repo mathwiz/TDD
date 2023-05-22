605cdff0958f42cd6b56b46187587344d4c6e820
Yohan Lee <yohanlee@yahoo.com>
2023-05-22 08:02:35 -0400

    racket command line

--- racket/hello.rkt
+++ racket/hello.rkt
@@ -1,4 +1,4 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-reader.ss" "lang")((modname hello) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
-(define square (lambda (x) (* x x)))
+#lang racket
+"Hello, world!"
