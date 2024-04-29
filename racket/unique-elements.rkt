54031ac47a1c8a717d9532be0a3e8f69905bb1d7
Yohan Lee <yohanlee@yahoo.com>
2024-04-28 21:39:28 -0400

    unique-elements

--- racket/unique-elements.rkt
+++ racket/unique-elements.rkt
@@ -1,6 +1,7 @@
 #lang racket
 (define lst '(1 2 3 4 4 5 6 6 7 8 8 8 8))
 (define ulst '(1 2 3))
+(define lst2 '(1 1 3))
 
 (define (has-only-unique-elements-p lst)
   (or (null? lst)
@@ -20,6 +21,22 @@
     (recur lst '() '()))
 )
 
+(define (my-remove-duplicates lst)
+  (letrec ((recur (lambda (current seen acc)
+                    (cond ((null? current) acc)
+                          ((member (car current) seen)
+                           (recur (cdr current) seen acc))
+                          (else
+                           (recur (cdr current) (cons (car current) seen) (cons (car current) acc)))))))
+    (recur lst '() '()))
+)
+
+
 (get-unique-elements lst)
+(get-unique-elements lst2)
 (get-unique-elements ulst)
 (get-unique-elements '())
+(my-remove-duplicates lst)
+(my-remove-duplicates lst2)
+(my-remove-duplicates ulst)
+(my-remove-duplicates '())
